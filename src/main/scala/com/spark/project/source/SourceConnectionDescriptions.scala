2e7f6d348302f4c9ec43f5173e66204144c8a415
JacksonGibsonESP <nikich911@gmail.com>
2019-02-08 11:17:54 +0300

    Добавил обработку ошибки параметра системы.

--- src/main/scala/com/spark/project/source/SourceConnectionDescriptions.scala
+++ src/main/scala/com/spark/project/source/SourceConnectionDescriptions.scala
@@ -5,15 +5,17 @@ import scala.collection.immutable.Map
 class SourceConnectionDescriptions {
 
   val a = Map(
-    "kih" -> "localhost:49161:xe:dbtypeoracle",
-    "khd" -> "localhost:1433::dbtypemssql",
+    "kih" -> "kiname:kiport:kisid:dbtypeoracle",
+    "khd" -> "khname:khport:khsid:dbtypemssql",
     "another" -> "anname:anport:ansid:dbtypeterad")
 
-  def findname(bname: String) = {
-
-    var b = a(bname)
-
-    b.split(":").toArray
-
+  def findname(bname: String): Array[String] = {
+    try {
+      val b = a(bname)
+      b.split(":")
+    } catch {
+      case e : Throwable =>
+        throw new Exception("Система " + bname + " отсутствует в справочнике.", e)
+    }
   }
 }
