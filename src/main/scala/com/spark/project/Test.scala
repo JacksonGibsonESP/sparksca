d715dae31b7977745bab4f1d58f5360eaaf6bf39
Nikita <nikich911@gmail.com>
2019-02-09 12:12:20 +0300

    Эксперименты с кодировками

--- src/main/scala/com/spark/project/Test.scala
+++ src/main/scala/com/spark/project/Test.scala
@@ -1,60 +1,56 @@
 package com.spark.project
-import java.nio.charset.CodingErrorAction
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, date_format, round, substring, to_date, unix_timestamp}
+import org.apache.spark.sql.functions.{col, date_format, round, substring}
 
-import scala.io.Codec
 
 object Test extends App {
   Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-  if (args.length == 1) {
-//      println(s"${args(0)}")
-
-//    implicit val codec = Codec("CP1251")
-//    codec.onMalformedInput(CodingErrorAction.REPLACE)
-//    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-
-      val spark = SparkSession
+  if (args.length > 0) {
+    val spark = SparkSession
 //        .builder.master("local[1]")
-        .builder()
-        .appName("Test")
-        .getOrCreate()
+      .builder()
+      .appName("Test")
+      .getOrCreate()
 
-      val path: String = args(0)
-
+    val path: String = args(0)
     println(path)
 
-      val df = spark.read
-        .format("com.databricks.spark.csv")
-        .option("delimiter", ";")
-        .option("encoding", "CP1251")
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .load(path)
+    //CP866, CP1251, UTF8
+    val encoding: String = args(1)
+    println(encoding)
 
+    val df = spark.read
+      .format("com.databricks.spark.csv")
+      .option("delimiter", ";")
+      .option("encoding", encoding)
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load(path)
 
-      val res = df
-        .withColumn("msg", substring(col("msg"), 2, 4))
-        .withColumn("summ", round(col("summ").divide(2)))
-        .withColumn("date", date_format(col("date"), "01-MM-yyyy"))
-//        .withColumn("date", to_date(unix_timestamp(col("date"), "yyyy-MM").cast("timestamp")))
-      res.show()
-      res.printSchema()
+    val res = df
+      .withColumn("msg", substring(col("msg"), 2, 4))
+      .withColumn("summ", round(col("summ").divide(2)))
+      .withColumn("date", date_format(col("date"), "01-MM-yyyy"))
+//      .withColumn("date", to_date(unix_timestamp(col("date"), "yyyy-MM").cast("timestamp")))
+    res.show()
+    res.printSchema()
 
-      val options = Map("path" -> "/tmp/test") // for me every database has a different warehouse. I am not using the default warehouse. I am using users' directory for warehousing DBs and tables
-      //and simply write it!
-      res.write.options(options).saveAsTable("db_name.table_name")
+    spark.sql("CREATE DATABASE IF NOT EXISTS test")
+    spark.sql("DROP TABLE IF EXISTS test.table_test")
 
-//      res.createGlobalTempView("test")
+    val options = Map("path" -> "/tmp/test")
+    res.write.options(options).saveAsTable("test.table_test")
 
-//      res.explain(true)
-
-//      spark.sql("SELECT * FROM global_temp.test").show()
-//      spark.sql("EXPLAIN SELECT * FROM global_temp.test").show(500, false)
+//    res.createGlobalTempView("test")
+//
+//    res.explain(true)
+//
+//    spark.sql("SELECT * FROM global_temp.test").show()
+//    spark.sql("EXPLAIN SELECT * FROM global_temp.test").show(500, false)
   }
   else
     println("No arguments provided")
